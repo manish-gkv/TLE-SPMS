d3fd93fb614aac56e949fb368ca136bafe8d9b54
manish-gkv <manishkumarslmp@gmail.com>
2025-06-17 20:11:32 +0530

    Added Email Service

---
+++ backend/src/services/email.js
@@ -0,0 +1,33 @@
+import nodemailer from "nodemailer";
+
+import { EMAIL, EMAIL_PASSWORD } from "../utility/constant.js";
+
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: EMAIL,
+        pass: EMAIL_PASSWORD,
+    },
+});
+
+const mailService = {
+    sendMail: async (to, inactiveDays) => {
+        const mailOptions = {
+            from: {
+                name: 'TLE Notification',
+                address: EMAIL
+            },
+            to,
+            subject: "Inactivity Alert",
+            text: `You have been inactive for ${inactiveDays} days. Please log in to your account to avoid account suspension.`,
+        };
+        try {
+            await transporter.sendMail(mailOptions);
+            console.log("Email sent successfully to:", to);
+        } catch (error) {
+            console.error("Error sending email:", error);
+        }
+    }
+}
+
+export default mailService;
